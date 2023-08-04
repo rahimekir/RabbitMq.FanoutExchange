34e7e0bdf7e2910c4a5972ef7de61096674779fe
RAHIME KIR <RAHIME.KIR@lcwaikiki.com>
2023-08-04 11:57:59 +0300

    Add project files.

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            // channel.QueueDeclare("hello-queue", true, false, false); => kuyruğu artık subscribe oluşturacak 
+            //durable :true => kuyruklar hiç bi zaman kaybolmaz. fiziksel olarak tutulur. 
+
+            channel.ExchangeDeclare("logs-fanout",type: ExchangeType.Fanout,durable:true);  //=>Exchange oluşturma işlemi
+
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"log {x} ";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+
+                //channel.BasicPublish(string.Empty, "hello-queue", null, messageBody); => eskisi
+                channel.BasicPublish("logs-fanout", "", null, messageBody);  //eskisinde direkt kuyruğa gönderdiğimiz içi  kuyruğun ismini vermiştik burdda boş kalacak.
+
+                Console.WriteLine($"Mesaj gönderilmiştir.: {message}");
+            }
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMq.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+   
+
+            //kuyruk oluşturma işlemi --isimleri aynı olmamalı 3 instance oluşturursam üçü de aynı instancea bağlanır 
+            var randomQueueName = channel.QueueDeclare().QueueName; // random kuyruk oluşturma propertysi 
+
+            //subscribe ilgili exchange e bind işlemi bittikten sonra kuyruğu silsin diye kuyruk declare etmiyoruz bind ediyoruz
+            channel.QueueBind(randomQueueName,"logs-fanout","",null);
+
+
+
+            channel.BasicQos(0, 1, false);
+            var subscriber = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(randomQueueName, false, subscriber);
+            Console.WriteLine("Loglar dinleniyor .." );
+
+
+            subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500); 
+                Console.WriteLine("Gelen mesaj : " + message);
+
+                channel.BasicAck(e.DeliveryTag, false); 
+            };
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
